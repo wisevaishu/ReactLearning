1599c87bd3aa58c7ae8b1640592d5e16503a2af2
wisevaishu <vpm.win@gmail.com>
2025-07-02 10:18:49 -0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 // import Footer from './Footer.jsx';
 // import Food from './Food.jsx';
 // import Card from './Card.jsx';
-import Button from './Button.jsx';
+//import Button from './Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
 
   return(
     <>
-      <Button/>
-      {/* <Header/>
+      <Student name="Welcome" age={39} isStudent={false}/> 
+      <Student name="Patrick" age={42} isStudent={true}/>
+      <Student name="Mohnish" age={10} isStudent={true}/>
+      <Student name="Sriman" age={14} isStudent={true}/>
+      <Student name="Prabhu"/> 
+      {/*<Button/>
+      <Header/>
       <Food/>
       <Footer/>
       <Card/>
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,8 +1,24 @@
-function Student(props){
+import PropTypes from 'prop-types';
+function Student({ name = "Guest", age = 0, isStudent = false }){
+     
     return(
-        <div>
-            
+        <div className="student">
+            <p>Name: {name}</p>
+            <p>Age : {age}</p>
+            <p>Student : {isStudent ? "Yes" : "No"}</p>
         </div>
     );
 }
+
+Student.propTypes = {
+name: PropTypes.string,
+age: PropTypes.number,
+isStudent: PropTypes.bool,
+}
+
+// Student.defaultProps = {
+//     name: "Guest",
+//     age : 0,
+//     isStudent: false,
+// }
 export default Student
